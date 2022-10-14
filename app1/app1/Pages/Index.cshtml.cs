8fc56e928c92da67f055fd1b44d75027b6877bf9
Milena002 <22143664@bucks.ac.uk>
2022-10-14 22:43:22 +0100

    all steps corrected and done

--- app1/app1/Pages/Index.cshtml.cs
+++ app1/app1/Pages/Index.cshtml.cs
@@ -1,20 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Extensions.Logging;
 
 namespace app1.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
+        [BindProperty]
+        public string FullName { get; set; }
+      
+        public readonly ILogger<IndexModel> _logger;
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
-
         public void OnGet()
         {
+            FullName = "Milena Michalska";
+        }
+        public void OnPost()
+        {
+            if (String.IsNullOrWhiteSpace(FullName))
+            {
+                ViewData["NoName"]="Name is missing!";
+                FullName = "Anonymous";
+            }
+            else
+            {
+                ViewData["Message"] = "Name is Registered";
 
+            }
+
+           
         }
     }
 }

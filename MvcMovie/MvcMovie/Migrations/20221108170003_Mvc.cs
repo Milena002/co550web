52eea12b55d73644a4900c630c6b66bc64927076
Milena002 <22143664@bucks.ac.uk>
2022-11-10 17:54:36 +0000

    all steps done

---
+++ MvcMovie/MvcMovie/Migrations/20221108170003_Mvc.cs
@@ -0,0 +1,57 @@
+﻿
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mvc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movies",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Genre",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public enum Genres
+    {
+        Comedy, 
+        Drama,
+        Animated,
+        Horror
+    }
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(60)]
+        public string Title { get; set; } = String.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public Genres Genre { get; set; }
+
+        [Range(2.99, 7.99)]
+        public decimal Price { get; set; }
+    }
+}

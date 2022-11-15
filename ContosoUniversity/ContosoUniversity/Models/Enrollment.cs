333fcd38e0cd5c2b97eb76a79a88a896e5cfc957
Milena002 <22143664@bucks.ac.uk>
2022-11-15 19:55:31 +0000

    completed

---
+++ ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grades
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+
+        public int CourseID { get; set; }
+
+        public int StudentID { get; set; }
+
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Nullable<Grades> Grade { get; set; }
+
+        // Navigation propetries
+        public virtual Course Course { get; set; }
+
+        public virtual Student Student { get; set; }
+    }
+}
